1b17508486234e68ab437ed5cfdb6ceb1f57b013
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2026-03-23 12:40:02 +0530

    chore: remove auto-generated Sanity runtime files

--- .sanity/runtime/app.js
+++
@@ -1,8 +0,0 @@
-import studioConfig from "../../sanity.config.ts"
-import {renderStudio} from "sanity"
-
-renderStudio(
-  document.getElementById("sanity"),
-  studioConfig,
-  {reactStrictMode: false, basePath: "/"}
-)
