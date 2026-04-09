ece3529c9d0ce612c8599e7c8cb089eb0f430bbe
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2026-04-09 14:38:53 +0530

    chore: clean up next.config.js by removing commented-out code and ensuring proper configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,34 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: { remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.sanity.io',
+//       },
+//     ],
+//     unoptimized: true },
+//   // trailingSlash: true,
+//   compress: true,
+//   async headers() {
+//     return [
+//       {
+//         source: '/blogs',
+//         headers: [
+//           {
+//             key: 'Cache-Control',
+//             value: 'public, s-maxage=60, stale-while-revalidate=120',
+//           },
+//         ],
+//       },
+//     ]
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
   images: { remotePatterns: [
       {
         protocol: 'https',
@@ -13,6 +38,19 @@ const nextConfig = {
     unoptimized: true },
   // trailingSlash: true,
   compress: true,
+  async headers() {
+    return [
+      {
+        source: '/blogs',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=60, stale-while-revalidate=120',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
